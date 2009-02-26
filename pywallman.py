e9756a521e11057998c61d06e3e7076219381fba
Sharn <go__cart91@hotmail.com>
2009-02-25 18:58:43 -0700

    Another dirty hack to name the saved file - rsplit and split to find actual filename.

--- pywallman.py
+++ pywallman.py
@@ -16,7 +16,7 @@ default_savedir = "~/pictures/wallpapers/test/"
 
 parser = OptionParser()
 parser.add_option("-g", "--genre", dest="genre", default="", help="Define which 'genre' or subdirectory to save a wallpaper in")
-parser.add_option("-f", "--file", dest="filename", help="Changes output name of file downloaded", metavar="FILE")
+parser.add_option("-f", "--file", dest="filename", default="", help="Changes output name of file downloaded", metavar="FILE")
 parser.add_option("-u", "--url", dest="fetch_url", default="", help="Set the url from which the script will fetch an image")
 parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=True, help="Don't print status messages to stdout")
 parser.add_option("-l", "--local", action="store_true", dest="local", default=False, help="Performs actions on local wallpapers")
@@ -37,9 +37,24 @@ def downloadWall(url, path, name):
 	urllib.urlretrieve(url, path)
 
 def checkPath():
+	global full_path
 	full_path = os.path.expanduser(default_savedir + options.genre)
 
 	if os.path.exists(full_path) and os.path.isdir(full_path):
+		if not full_path[-1] == "/":
+			# If no trailing slash, add one
+			full_path = full_path + "/"
+
+		if not options.filename == "":
+			# If a filename was passed, use it
+			full_path = full_path + options.filename
+		else:
+			# Else get the filename from the url
+			filenamesplit = options.fetch_url.rsplit("/", 1)
+			filenamesplit2 = filenamesplit[1].split("&", 1)
+			filename = filenamesplit2[0]
+			full_path = full_path + filename
+
 		scriptStatus("Path: " + full_path)
 		return True
 	else:
@@ -48,5 +63,4 @@ def checkPath():
 
 if not options.fetch_url == "" and checkPath():
 	scriptStatus("Found url on cmd line - parsing and checking for genre")
-	full_path = os.path.expanduser(default_savedir + options.genre + options.filename)	
 	downloadWall(options.fetch_url, full_path, options.filename)
