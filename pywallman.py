84e786f69342e75ee5c8c38d471030bd0c9de330
Sharn <go__cart91@hotmail.com>
2009-02-26 20:25:12 -0700

    Updated to use library functions, filename should be hard to mess up.

--- pywallman.py
+++ pywallman.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import string
-import sys
-import urllib
-from os.path import isdir, exists, expanduser
+import os, string, sys, urllib, urlparse
 from time import localtime
 from optparse import OptionParser
 
@@ -16,7 +12,7 @@ default_savedir = "~/pictures/wallpapers/test/"
 
 parser = OptionParser()
 parser.add_option("-g", "--genre", dest="genre", default="", help="Define which 'genre' or subdirectory to save a wallpaper in")
-parser.add_option("-f", "--file", dest="filename", default="", help="Changes output name of file downloaded", metavar="FILE")
+parser.add_option("-f", "--file", dest="filename", default="", help="Changes output name of file downloaded - make SURE you set correct extension", metavar="FILE")
 parser.add_option("-u", "--url", dest="fetch_url", default="", help="Set the url from which the script will fetch an image")
 parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=True, help="Don't print status messages to stdout")
 parser.add_option("-l", "--local", action="store_true", dest="local", default=False, help="Performs actions on local wallpapers")
@@ -32,35 +28,42 @@ def scriptStatus(status):
 	else:
 		return False
 
-def downloadWall(url, path, name):
-# Actually downloads the wallpaper file, given 
+def downloadWall(url, path):
+	# Actually downloads the wallpaper file, given 
 	urllib.urlretrieve(url, path)
 
 def checkPath():
 	global full_path
-	full_path = os.path.expanduser(default_savedir + options.genre)
+	global options
+	full_path = os.path.expanduser(os.path.join(default_savedir,  options.genre))
 
 	if os.path.exists(full_path) and os.path.isdir(full_path):
-		if not full_path[-1] == "/":
-			# If no trailing slash, add one
-			full_path = full_path + "/"
-
 		if not options.filename == "":
 			# If a filename was passed, use it
-			full_path = full_path + options.filename
+			full_path = os.path.join(full_path, options.filename)
 		else:
 			# Else get the filename from the url
-			filenamesplit = options.fetch_url.rsplit("/", 1)
-			filenamesplit2 = filenamesplit[1].split("&", 1)
-			filename = filenamesplit2[0]
-			full_path = full_path + filename
+			options.filename = os.path.basename(urlparse.urlparse(options.fetch_url).path)
+			full_path = os.path.join(full_path, options.filename)
 
-		scriptStatus("Path: " + full_path)
-		return True
+		if os.path.exists(full_path):
+			scriptStatus("File already exists")
+			return False
+		else:
+			scriptStatus("Path: " + full_path)
+			return True
 	else:
-		scriptStatus("Ewwy, path doesn't exist or isn't a directory")
+		scriptStatus("Path/Genre doesn't exist or isn't a directory")
 		return False
 
+def pickleData(path, description, date):
+	if description == "":
+		description
+
+
+# Main program logic
 if not options.fetch_url == "" and checkPath():
 	scriptStatus("Found url on cmd line - parsing and checking for genre")
-	downloadWall(options.fetch_url, full_path, options.filename)
+	downloadWall(options.fetch_url, full_path)
+else:
+	scriptStatus("Nothing to do - exiting")
